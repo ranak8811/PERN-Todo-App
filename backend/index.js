f2c6fff5f7d4ace5d95a5c87c5465b96f5193596
Anwar_Hossain <ranakrphone@gmail.com>
2026-06-11 15:19:57 +0600

    cors url updated

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,13 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// app.use(cors({
-//   origin: "https://frontend-6j0r.onrender.com"
-// }));
+app.use(
+  cors({
+    origin: "https://pern-todo-app-frontend-a1r0.onrender.com",
+  }),
+);
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use("/todos", todoRoutes);
